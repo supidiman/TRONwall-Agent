73d5f09d6361a6e1cd21d92894860c35051721b7
zehraakucuk13 <zehraakucuk13@gmail.com>
2025-12-28 00:19:31 +0300

    Update and rename starts_system.py to start_system.py

---
+++ start_system.py
@@ -0,0 +1,14 @@
+
+import subprocess
+import os
+import sys
+
+def launch():
+    print("🛡️ TRONwall Sistemi Başlatılıyor...")
+    # WAF ve AI modüllerini arka planda başlatır
+    subprocess.Popen([sys.executable, "waf_core/server.py"])
+    subprocess.Popen([sys.executable, "ai_agent/decision_engine.py"])
+    print("✅ Sistemler Aktif. Analiz terminalden izlenebilir.")
+
+if __name__ == "__main__":
+    launch()
