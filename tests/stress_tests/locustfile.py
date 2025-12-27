145ac183ed66070d26d68d0090051a2941286199
zehraakucuk13 <zehraakucuk13@gmail.com>
2025-12-28 00:20:27 +0300

    Update locustfile.py

--- tests/stress_tests/locustfile.py
+++ tests/stress_tests/locustfile.py
@@ -1,1 +1,8 @@
+from locust import HttpUser, task, between
 
+class TronwallUser(HttpUser):
+    wait_time = between(0.1, 0.5) # 100-500 kullanıcı simülasyonu için düşük bekleme
+
+    @task
+    def visit(self):
+        self.client.get("/")
