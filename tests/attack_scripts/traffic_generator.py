e77e02d3cfaa9cda9af0711f1e5b120b4534595b
zehraakucuk13 <zehraakucuk13@gmail.com>
2025-12-29 03:24:17 +0300

    Update traffic_generator.py

--- tests/attack_scripts/traffic_generator.py
+++ tests/attack_scripts/traffic_generator.py
@@ -7,21 +7,21 @@ ATTACKS = ["<script>alert(1)</script>", "' OR 1=1 --", "../../etc/passwd"]
 NORMALS = ["Anasayfa", "Hakkimizda", "Urunler", "Iletisim"]
 
 def start_bot():
-    print("🚀 TRONwall Saldırı Botu Aktif...")
+    print("✈︎ TRONwall Saldırı Botu Aktif...")
     while True:
         # %20 Saldırı, %80 Normal trafik ayarı
         if random.random() < 0.20:
             payload = random.choice(ATTACKS)
-            label = "🔥 SALDIRI"
+            label = "✖ DANGER"
         else:
             payload = random.choice(NORMALS)
-            label = "✅ NORMAL "
+            label = "✔ SECURE "
         
         try:
             requests.get(URL, params={"data": payload})
             print(f"{label}: {payload[:15]}...")
         except:
-            print("❌ Sunucuya erişilemiyor!")
+            print("! ! ! Sunucuya erişilemiyor!")
         
         time.sleep(random.uniform(0.1, 0.2)) # Saniyede ~5-10 istek
 
